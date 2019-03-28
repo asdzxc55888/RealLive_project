78f5bcb8ec39afc328c2dbc9dad200fe317f8a40
weiChen <asdzxc55888@gmail.com>
2019-03-28 22:17:16 +0800

    index app add

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from indexPage.views import indexPage_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indexPage_view),
 ]
