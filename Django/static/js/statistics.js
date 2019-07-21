124bbff88538a1dea871be33b8a14b9bbc9b4a6a
Ting <t105590015@ntut.org.tw>
2019-07-21 06:05:59 -0700

    update statistics page

--- Django/static/js/statistics.js
+++ Django/static/js/statistics.js
@@ -1,3 +1,4 @@
+// 製作圖表
 var emotion = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'];
 
 var ctx = document.getElementById('myChart');
@@ -36,4 +37,64 @@ var myChart = new Chart(ctx, {
 			}]
 		}
 	}
+});
+
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+// playerlist
+$(function(){
+	var thumbSize = 'large',		// 設定要取得的縮圖是大圖還是小圖
+		imgWidth = '200',			// 限制圖片的寬
+		imgHeight = '150',			// 限制圖片的高
+		apiKey = 'AIzaSyBmta_M-BHZ6UixGn7HsI5owf--BBS-6-0';
+
+	$('.connect_button').click(function(){
+		var address = prompt("請輸入網址\n(ex. https://www.youtube.com/watch?v=[your video ID])", "");
+		var _type = (thumbSize == 'large') ? 0 : 2;
+
+		try {
+			// 取得 vid
+			var vid = address.match('[\\?&]v=([^&#]*)')[1];
+
+			// 取得 title
+			var title = 'None';
+			$.ajax({
+		        url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + vid + '&key=' + apiKey,
+		        async: false,
+		        dataType: 'json',
+		        success: function(data) {
+					console.log("Successfully obtained information.");
+					title = (data['items'][0]['snippet']['title']);
+		        },
+				error: function() {
+    				console.log("Failed to get the data, see network tab for response details.");
+  				}
+		    });
+
+			// 取得縮圖
+			var thumbUrl = "http://img.youtube.com/vi/" + vid + "/" + _type + ".jpg";
+
+			var html = '<li class="row"><a class="col-md-6" href="' + address + '">';
+			html += '<img src="' + thumbUrl + '" alt="' + title + '" title="' + title + '" width="' + imgWidth + '" height="' + imgHeight + '" />';
+			html += '</a><p class="col-md-6" >' + title + '</p></li>';
+
+			var option = htmlToElement(html);
+			$(option).children('a').click(function() {
+				// 當點擊到圖片時就轉換成 YouTube 影片
+				$('#player').attr('src', 'https://www.youtube.com/embed/' + vid + '?rel=0&autoplay=1');
+
+				return false;
+			});
+
+			$('.playlist').append(option);
+		}
+		catch {
+			alert('網址無法連接');
+		};
+	});
 });
