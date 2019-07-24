d571e7cbcb2bc197bb8637dbd5640213e1ca236c
Ting <t105590015@ntut.org.tw>
2019-07-24 10:32:30 -0700

    html use django templates

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own
     'indexPage',
     'livePage',
     'statisticsPage',
@@ -124,5 +126,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'static/'),
         ]
