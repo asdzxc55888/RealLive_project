2fe047a3b9576cc638725ea1880c839470cae7ec
Ting <t105590015@ntut.org.tw>
2019-08-01 09:37:34 -0700

    update statistics chart

--- Django/static/js/statistics.js
+++ Django/static/js/statistics.js
@@ -2,33 +2,53 @@ function playVideo(vid) {
 	$('#player').attr('src', 'https://www.youtube.com/embed/' + vid);
 }
 
-function createChart(time, data) {
-	var emotion = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral'];
-
+function createChart(time, angryData, disgustData, fearData, happyData, sadData, surpriseData, neutralData) {
 	var ctx = document.getElementById('myChart');
 	var myChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			labels: time,
 			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
+				label: 'Angry',
+				data: angryData,
+				backgroundColor: 'rgba(255, 99, 132, 0.5)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Disgust',
+				data: disgustData,
+				backgroundColor: 'rgba(75, 192, 192, 0.5)',
+				borderColor: 'rgba(75, 192, 192, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Fear',
+				data: fearData,
+				backgroundColor: 'rgba(153, 102, 255, 0.5)',
+				borderColor: 'rgba(153, 102, 255, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Happy',
+				data: happyData,
+				backgroundColor: 'rgba(255, 159, 64, 0.7)',
+				borderColor: 'rgba(255, 159, 64, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Sad',
+				data: sadData,
+				backgroundColor: 'rgba(54, 162, 235, 0.5)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Surprise',
+				data: surpriseData,
+				backgroundColor: 'rgba(255, 205, 86, 0.5)',
+				borderColor: 'rgba(255, 205, 86, 1)',
+				borderWidth: 1
+			}, {
+				label: 'Neutral',
+				data: neutralData,
+				backgroundColor: 'rgba(201, 203, 207, 0.8)',
+				borderColor: 'rgba(201, 203, 207, 1)',
 				borderWidth: 1
 			}]
 		},
--- Django/statisticsPage/views.py
+++ Django/statisticsPage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from livePage.models import EmotionData, VideoRecord
+import json
 
 # Create your views here.
 class statisticsView(View):
@@ -17,10 +18,42 @@ class statisticsView(View):
 
     def post(self, request, *args, **kwargs):
         _videos = None
+        _vid = request.POST.get("vid", "")
+        _time = []
+        _angryData = []
+        _disgustData = []
+        _fearData =[]
+        _happyData = []
+        _sadData = []
+        _surpriseData = []
+        _neutralData = []
         if request.user.is_authenticated:
             _videos = VideoRecord.objects.filter(userId = request.user)
+
+            # get emotional data
+            video = VideoRecord.objects.get(vid = _vid)
+            emotionData = EmotionData.objects.filter(vid = video)
+            for data in emotionData:
+                _time.append(data.time.strftime("%H:%M:%S"))
+                _angryData.append(data.Angry)
+                _disgustData.append(data.Disgust)
+                _fearData.append(data.Fear)
+                _happyData.append(data.Happy)
+                _sadData.append(data.Sad)
+                _surpriseData.append(data.Surprise)
+                _neutralData.append(data.Neutral)
+
+
         context = {
             'videos': _videos,
-            'vid': request.POST.get("vid", ""),
+            'vid': _vid,
+            'time': json.dumps(_time),
+            'angryData': _angryData,
+            'disgustData': _disgustData,
+            'fearData': _fearData,
+            'happyData': _happyData,
+            'sadData': _sadData,
+            'surpriseData': _surpriseData,
+            'neutralData': _neutralData,
         }
         return render(request, self.template_name, context)
